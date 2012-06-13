e918caca96c648521bae7b37c1c07c700d27b788
peiffer <>
2012-06-13 09:37:33 +0000

    doxygen info comments

--- include/Selection.h
+++ include/Selection.h
@@ -8,31 +8,62 @@
 #include "ObjectHandler.h"
 #include "../../core/include/SLogger.h"
 
+/**
+ *  @short basic class for selection modules
+ *  @author Thomas Peiffer
+ */
+
 class SelectionModule{
  public:
+  //Default constructor
   SelectionModule(){};
+  //Default destructor
   virtual ~SelectionModule(){};
    
   virtual bool pass(BaseCycleContainer*)=0;
   virtual std::string description()=0;
 };
 
+/**
+ *  @short basic class for a selection
+ *
+ *         contains a list of SelectionModules
+ *         and produces cut flow tables
+ *
+ *  @author Thomas Peiffer
+ */
 
 class Selection{
  public:
   
   //Selection(){};
+  /// Default constructor
   Selection(std::string name);
+  /// Default destructor
   ~Selection(){};
   
+  /// add a new SelectionModule to the event selection
   void addSelectionModule(SelectionModule*);
+  /// remove all booked SelectionModules
   void clearSelectionModulesList();
+  /// set all entries in the cut flow table to 0
   void resetCutFlow();
+
+  /**
+   * loop over all booked SelectionModules and call the pass() routine of each module for a given BaseCycleContainer
+   * @see passSelection()
+   * @see SelectionModule::pass(BaseCycleContainer*)
+   */
   bool passSelection(BaseCycleContainer *bcc);
   bool passInvertedSelection(BaseCycleContainer *bcc);
+  /**
+   * same as passSelection(BaseCycleContainer *bcc) but takes BaseCycleContainer from ObjectHandler
+   * @see passSelection(BaseCycleContainer *bcc)
+   */
   bool passSelection();
   bool passInvertedSelection();
 
+  /// print the cut-flow table for the booked SelectionModules, to be called in the EndInputData routine of a cycle
   void printCutFlow();
 
   TString GetName() {return m_name;}
