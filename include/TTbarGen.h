55e3f62e4294d25916e1ae6dda936c863d49e890
mmeyer <>
2012-07-03 15:26:22 +0000

    decay channel

--- include/TTbarGen.h
+++ include/TTbarGen.h
@@ -18,12 +18,30 @@ class TTbarGen
   /// Default destructor
   ~TTbarGen();
 
+  enum E_DecayChannel{
+    e_had, 
+    e_ehad, 
+    e_muhad,
+    e_tauhad,
+    e_ee,  
+    e_mumu,
+    e_tautau,
+    e_emu,
+    e_etau,
+    e_mutau  
+  };
+
   GenParticle Top(); 
   GenParticle Antitop();
   GenParticle WTop(); 
   GenParticle WAntitop(); 
   GenParticle bTop(); 
   GenParticle bAntitop(); 
+  GenParticle Wdecay1(); 
+  GenParticle Wdecay2(); 
+  GenParticle WMinusdecay1(); 
+  GenParticle WMinusdecay2();
+  E_DecayChannel DecayChannel();
   
  private:
 
@@ -33,7 +51,20 @@ class TTbarGen
   GenParticle m_WAntitop;
   GenParticle m_bTop; 
   GenParticle m_bAntitop;
-  
+  GenParticle m_Wdecay1; 
+  GenParticle m_Wdecay2; 
+  GenParticle m_WMinusdecay1; 
+  GenParticle m_WMinusdecay2; 
+  int m_pdgId1;
+  int m_pdgId2;
+  int m_indexW;
+  int m_indexb;
+  int m_index1;
+  int m_index2;
+
+ E_DecayChannel m_type;
+
+
 }; // class TTbarGen
 
 
--- src/TTbarGen.cxx
+++ src/TTbarGen.cxx
@@ -10,32 +10,32 @@ TTbarGen::TTbarGen()
   for(unsigned int i=0; i<bcc->genparticles->size(); ++i)
    {
      GenParticle genp = bcc->genparticles->at(i);
+     m_pdgId1 = 0;
+     m_pdgId2 = 0;
      if ( genp.pdgId() == 6 ) 
        {
 	 m_Top = genp;
 	 if (genp.daughter(bcc->genparticles,1) && genp.daughter(bcc->genparticles,2))
 	   {
-	     int indexW;
-	     int indexb;
 	     if (genp.daughter(bcc->genparticles,1)->pdgId() == 24) 
 	       {
-		 indexW = genp.daughter(bcc->genparticles,1)->index();
-		 m_WTop = bcc->genparticles->at(indexW);
+		 m_indexW = genp.daughter(bcc->genparticles,1)->index();
+		 m_WTop = bcc->genparticles->at(m_indexW);
 	       }
 	     if (genp.daughter(bcc->genparticles,1)->pdgId() == 5)  
 	       {
-		 indexb = genp.daughter(bcc->genparticles,1)->index();
-		 m_bTop = bcc->genparticles->at(indexb);
+		 m_indexb = genp.daughter(bcc->genparticles,1)->index();
+		 m_bTop = bcc->genparticles->at(m_indexb);
 	       }
 	     if (genp.daughter(bcc->genparticles,2)->pdgId() == 24)
 	       {
-		 indexW = genp.daughter(bcc->genparticles,2)->index();
-		 m_WTop = bcc->genparticles->at(indexW);
+		 m_indexW = genp.daughter(bcc->genparticles,2)->index();
+		 m_WTop = bcc->genparticles->at(m_indexW);
 	       }
 	     if (genp.daughter(bcc->genparticles,2)->pdgId() == 5)  
 	       {
-		 indexb = genp.daughter(bcc->genparticles,2)->index();
-		 m_bTop = bcc->genparticles->at(indexb);
+		 m_indexb = genp.daughter(bcc->genparticles,2)->index();
+		 m_bTop = bcc->genparticles->at(m_indexb);
 	       }
 	   }
        }
@@ -44,28 +44,49 @@ TTbarGen::TTbarGen()
 	 m_Antitop = genp;
 	 if (genp.daughter(bcc->genparticles,1) && (genp.daughter(bcc->genparticles,2)))
 	   {
-	     int indexW;
-	     int indexb;
 	     if (genp.daughter(bcc->genparticles,1)->pdgId() == -24) 
 	       {
-		 indexW = genp.daughter(bcc->genparticles,1)->index();
-		 m_WAntitop = bcc->genparticles->at(indexW);
+		 m_indexW = genp.daughter(bcc->genparticles,1)->index();
+		 m_WAntitop = bcc->genparticles->at(m_indexW);
 	       }
 	     if (genp.daughter(bcc->genparticles,1)->pdgId() == -5)  
 	       {
-		 indexb = genp.daughter(bcc->genparticles,1)->index();
-		 m_bAntitop = bcc->genparticles->at(indexb);
+		 m_indexb = genp.daughter(bcc->genparticles,1)->index();
+		 m_bAntitop = bcc->genparticles->at(m_indexb);
 	       }
 	     if (genp.daughter(bcc->genparticles,2)->pdgId() == -24)
 	       {
-		 indexW = genp.daughter(bcc->genparticles,2)->index();
-		 m_WAntitop = bcc->genparticles->at(indexW);
+		 m_indexW = genp.daughter(bcc->genparticles,2)->index();
+		 m_WAntitop = bcc->genparticles->at(m_indexW);
 	       }
 	     if (genp.daughter(bcc->genparticles,2)->pdgId() == -5)  
 	       {
-		 indexb = genp.daughter(bcc->genparticles,2)->index();
-		 m_bAntitop = bcc->genparticles->at(indexb);
+		 m_indexb = genp.daughter(bcc->genparticles,2)->index();
+		 m_bAntitop = bcc->genparticles->at(m_indexb);
 	       }
+	   }
+       }
+     if ( genp.pdgId() == 24 && genp.mother(bcc->genparticles,1)->pdgId() == 6)
+       {
+	 if (genp.daughter(bcc->genparticles,1) && genp.daughter(bcc->genparticles,2))
+	   {
+	     m_index1 = genp.daughter(bcc->genparticles,1)->index();
+	     m_Wdecay1 = bcc->genparticles->at(m_index1);
+	     m_pdgId1= m_Wdecay1.pdgId();
+	     m_index2 = genp.daughter(bcc->genparticles,1)->index();
+	     m_Wdecay2 = bcc->genparticles->at(m_index2);
+	     
+	   }
+       }
+     if ( genp.pdgId() == -24 && genp.mother(bcc->genparticles,1)->pdgId() == -6)
+       {
+	 if (genp.daughter(bcc->genparticles,1) && (genp.daughter(bcc->genparticles,2)))
+	   {
+	     m_index1 = genp.daughter(bcc->genparticles,1)->index();
+	     m_WMinusdecay1 = bcc->genparticles->at(m_index1);
+	     m_pdgId2= m_WMinusdecay1.pdgId(); 
+	     m_index2 = genp.daughter(bcc->genparticles,1)->index();
+	     m_WMinusdecay2 = bcc->genparticles->at(m_index2);
 	   }
        }
    }
@@ -107,5 +128,43 @@ GenParticle TTbarGen::bAntitop()
   return m_bAntitop;
 } 
 
+GenParticle TTbarGen::Wdecay1()
+{
+  return m_Wdecay1;
+} 
 
+GenParticle TTbarGen::Wdecay2()
+{
+  return m_Wdecay2;
+} 
 
+GenParticle TTbarGen::WMinusdecay1()
+{
+  return m_WMinusdecay1;
+} 
+
+GenParticle TTbarGen::WMinusdecay2()
+{
+  return m_WMinusdecay2;
+} 
+
+TTbarGen::E_DecayChannel TTbarGen::DecayChannel()
+{
+  if ((abs(m_pdgId1)==11 || abs(m_pdgId1)==12) && (abs(m_pdgId2)==11 || abs(m_pdgId2)==12)) m_type = e_ee;
+  if ((abs(m_pdgId1)==13 || abs(m_pdgId1)==14) && (abs(m_pdgId2)==13 || abs(m_pdgId2)==14)) m_type = e_mumu;
+  if ((abs(m_pdgId1)==15 || abs(m_pdgId1)==16) && (abs(m_pdgId2)==15 || abs(m_pdgId2)==16)) m_type = e_tautau;
+  if ((abs(m_pdgId1)<=5) && (abs(m_pdgId2)<=5)) m_type = e_had;
+  if ((abs(m_pdgId1)==11 || abs(m_pdgId1)==12) && (abs(m_pdgId2)<=5)) m_type = e_ehad;
+  if ((abs(m_pdgId2)==11 || abs(m_pdgId2)==12) && (abs(m_pdgId1)<=5)) m_type = e_ehad;
+  if ((abs(m_pdgId1)==13 || abs(m_pdgId1)==14) && (abs(m_pdgId2)<=5)) m_type = e_muhad;
+  if ((abs(m_pdgId2)==13 || abs(m_pdgId2)==14) && (abs(m_pdgId1)<=5)) m_type = e_muhad;
+  if ((abs(m_pdgId1)==15 || abs(m_pdgId1)==16) && (abs(m_pdgId2)<=5)) m_type = e_tauhad;
+  if ((abs(m_pdgId2)==15 || abs(m_pdgId2)==16) && (abs(m_pdgId1)<=5)) m_type = e_tauhad;
+  if ((abs(m_pdgId1)==11 || abs(m_pdgId1)==12) && (abs(m_pdgId2)==13 || abs(m_pdgId2)==14)) m_type = e_emu;
+  if ((abs(m_pdgId2)==11 || abs(m_pdgId2)==12) && (abs(m_pdgId1)==13 || abs(m_pdgId1)==14)) m_type = e_emu;
+  if ((abs(m_pdgId1)==11 || abs(m_pdgId1)==12) && (abs(m_pdgId2)==15 || abs(m_pdgId2)==16)) m_type = e_etau;
+  if ((abs(m_pdgId2)==11 || abs(m_pdgId2)==12) && (abs(m_pdgId1)==15 || abs(m_pdgId1)==16)) m_type = e_etau;
+  if ((abs(m_pdgId1)==13 || abs(m_pdgId1)==14) && (abs(m_pdgId2)==15 || abs(m_pdgId2)==16)) m_type = e_mutau;
+  if ((abs(m_pdgId2)==13 || abs(m_pdgId2)==14) && (abs(m_pdgId1)==15 || abs(m_pdgId1)==16)) m_type = e_mutau;
+  return m_type;
+}
