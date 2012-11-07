e12ba8c591d3d48a49b1c99a92c205b04ced4755
mmeyer <>
2012-11-07 10:53:57 +0000

    HLT_IsoMu24

--- src/MCDataScaleFactors.cxx
+++ src/MCDataScaleFactors.cxx
@@ -24,25 +24,32 @@ double LeptonScaleFactors::GetWeight(){
   double IDfactor = 0;
   double isofactor=0;
   double sumofweights=0;
-
+  
   double Mu40triggerRunA[3] = {0.9799, 0.9621, 0.9851};
   double Mu40triggerRunB[3] = {0.9773, 0.9573, 0.9754};
   double Mu40triggerRunC[3] = {0.9817, 0.9640, 0.9973};
 
+  double IsoMu24triggerRunA[3] = {0.9560, 0.9528, 0.9809};
+  double IsoMu24triggerRunB[3] = {0.9798, 0.9618, 0.9814};
+  double IsoMu24triggerRunC[3] = {0.9841, 0.9688, 1.0021};
+
   double TightID_RunAB[3] = {0.9941, 0.9917, 0.9982};
   double TightID_RunC[3] = {0.9934, 0.9903, 0.9979};
+
+  double Isolation_RunAB[3] = {0.9923, 0.9979, 1.0019};
+  double Isolation_RunC[3] = {0.9978, 1.0005, 1.0044};
 
   EventCalc* calc = EventCalc::Instance();
 
   //Take lepton with highest transverse momentum as reference. Make sure that routine is called after selection of exactly one good lepton.
   Particle* primlep = calc->GetPrimaryLepton();
-
-  double eta = fabs(primlep->eta());
+  
+  double eta = primlep->eta();
 
   int etabin=0;
-  if(eta<0.9) etabin=0;
-  else if(eta>=0.9 && eta<1.2) etabin=1;
-  else if(eta>=1.2) etabin=2;
+  if(fabs(eta)<0.9) etabin=0;
+  else if(fabs(eta)>=0.9 && fabs(eta)<1.2) etabin=1;
+  else if(fabs(eta)>=1.2) etabin=2;
 
 
   for(unsigned int i=0; i<m_correctionlist.size(); ++i){
@@ -67,19 +74,25 @@ double LeptonScaleFactors::GetWeight(){
     }
     //isolated muons
     else if(m_correctionlist[i].first == "IsoMuonRunA"){
-          
+      triggerfactor += weight*IsoMu24triggerRunA[etabin];
+      isofactor+=weight*Isolation_RunAB[etabin];
+      IDfactor+= weight*TightID_RunAB[etabin];   
     }
     else if (m_correctionlist[i].first == "IsoMuonRunB"){
-
+      triggerfactor += weight*IsoMu24triggerRunB[etabin];
+      isofactor+=weight*Isolation_RunAB[etabin];
+      IDfactor+= weight*TightID_RunAB[etabin];
     }
     else if (m_correctionlist[i].first == "IsoMuonRunC"){
-
+      triggerfactor += weight*IsoMu24triggerRunC[etabin];
+      isofactor+=weight*Isolation_RunC[etabin];
+      IDfactor+= weight*TightID_RunC[etabin];
     }
-
+    
     else{
       std::cerr<< "No information found for lepton correction named " << m_correctionlist[i].first <<std::endl;
     }
-
+    
     sumofweights += m_correctionlist[i].second;
 
   }
