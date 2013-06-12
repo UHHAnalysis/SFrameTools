a51f3a8676e39c25ea08c0eacad00359d3f054ec
peiffer <>
2013-06-12 16:06:11 +0000

    build files from new sframe

--- proof/BUILD.sh
+++ proof/BUILD.sh
@@ -1,7 +1,12 @@
-# $Id$
+# $Id: BUILD.sh,v 1.1 2012/05/25 08:39:23 rkogler Exp $
 
 if [ "$1" = "clean" ]; then
     make distclean
+    exit 0
+fi
+
+if [ "x$ROOTPROOFLITE" != "x" ]; then
+    echo "Running on PROOF-Lite, skipping build"
     exit 0
 fi
 
